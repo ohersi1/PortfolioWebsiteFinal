c2c643548d97d1318ea1661f78af00b7f3949ca3
ohersi1 <osmanhersi@hotmail.co.uk>
2023-04-07 19:57:28 +0100

    continued styling the navbar and added a media query now starting on the hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,33 @@ import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
 	return (
 		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
-			<div className="">
-        <img src={Logo} alt="Logo Image" />
-      </div>
+			<div>
+				<img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+			</div>
+			{/* Menu */}
+
+			<ul className="hidden md:flex">
+				<li>Home</li>
+				<li>About</li>
+				<li>Skills</li>
+				<li>Work</li>
+				<li>Contact</li>
+			</ul>
+
+			{/* Hamburger Menu */}
+			<div className="hidden">
+				<FaBars />
+			</div>
+			{/* Mobile Menu */}
+			<ul className="hidden">
+				<li>Home</li>
+				<li>About</li>
+				<li>Skills</li>
+				<li>Work</li>
+				<li>Contact</li>
+			</ul>
+			{/* Social Icons */}
+			<div className="hidden"></div>
 		</div>
 	);
 };
