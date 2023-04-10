89bd3ce775742ab6de13aa9caf62333da7e34f66
ohersi1 <osmanhersi@hotmail.co.uk>
2023-04-10 22:20:44 +0100

    finished styling the navbar added media queries and mobile links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import Logo from "../assets/logo1.png";
+import React, { useState } from "react";
+import Logo from "../assets/logo2.png";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+	const [nav, setNav] = useState(false);
+	const handleClick = () => setNav(!nav);
 	return (
 		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
 			<div>
-				<img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+				<img
+					src={Logo}
+					alt="Logo Image"
+					style={{
+						height: "21px",
+						color: "white",
+						backgroundColor: "transparent",
+					}}
+				/>
 			</div>
 			{/* Menu */}
 
@@ -19,16 +29,23 @@ const Navbar = () => {
 			</ul>
 
 			{/* Hamburger Menu */}
-			<div className="hidden">
-				<FaBars />
+			<div onClick={handleClick} className="md:hidden z-10">
+				
+				{!nav ? <FaBars /> : <FaTimes />}
 			</div>
 			{/* Mobile Menu */}
-			<ul className="hidden">
-				<li>Home</li>
-				<li>About</li>
-				<li>Skills</li>
-				<li>Work</li>
-				<li>Contact</li>
+			<ul
+				className={
+					!nav
+						? "hidden"
+						: "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-centerabsolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+				}
+			>
+				<li className="py-6 text-4xl">Home</li>
+				<li className="py-6 text-4xl">About</li>
+				<li className="py-6 text-4xl">Skills</li>
+				<li className="py-6 text-4xl">Work</li>
+				<li className="py-6 text-4xl">Contact</li>
 			</ul>
 			{/* Social Icons */}
 			<div className="hidden"></div>
