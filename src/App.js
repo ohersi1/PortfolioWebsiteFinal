76ac34f181436da5068885b88ec2147779eb8c2a
ohersi1 <osmanhersi@hotmail.co.uk>
2023-04-16 19:54:51 +0100

    finished working on the skills component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Skills from "./components/Skills";
+
 
 function App() {
 	return (
@@ -8,6 +10,7 @@ function App() {
 			<Navbar />
 			<Home />
 			<About />
+      <Skills />
 		</div>
 	);
 }
