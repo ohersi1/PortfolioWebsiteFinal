b4f9964b364bb0c52e777862959ee9a5bf44f803
ohersi1 <osmanhersi@hotmail.co.uk>
2023-04-18 19:54:43 +0100

    finished the work component and styled it to be responsive with grid

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
-
+import Work from "./components/Work";
 
 function App() {
 	return (
@@ -10,7 +10,8 @@ function App() {
 			<Navbar />
 			<Home />
 			<About />
-      <Skills />
+			<Skills />
+			<Work />
 		</div>
 	);
 }
